9539d7b296d4ea34431157109b8ec17830f98acd
kawe-education <to379@drexel.edu>
2025-04-29 19:47:29 -0400

    initial commit of summaryCards

---
+++ src/helper/data.ts
@@ -0,0 +1,26 @@
+
+export const summaryCards = [
+    {
+        title: "Monthly Spending",
+        value: "$3,000",
+        icon: "💰"
+    },
+    {
+        title: "Investment Value",
+        value: "$100,000",
+        icon: "🚀"
+    },
+    {
+        title: "Best Performing Stock",
+        value: "TSLA",
+        icon: "📈"
+    },
+    {
+        title: "Savings Rate",
+        value: "25%",
+        icon: "💰"
+    },
+
+
+]
+    
