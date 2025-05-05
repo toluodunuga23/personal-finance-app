6c3ce04426a5ace24175b9151c560b38779b1578
kawe-education <to379@drexel.edu>
2025-05-04 20:56:15 -0400

    adding bar chart to page

--- src/app/my-stocks/page.tsx
+++ src/app/my-stocks/page.tsx
@@ -3,46 +3,170 @@ import Sidebar from "@/components/Sidebar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useApi } from "@/hooks/useApi";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartConfig = {
+  closePrice: {
+    label: "Close Price",
+    color: "blue",
+  },
+} satisfies ChartConfig;
 
 const MyStocks = () => {
-  const { data, isLoading, error, fetchData } = useApi();
+  const { data, isLoading, setIsLoading, fetchData, error, setError } =
+    useApi();
   const [searchQuery, setSearchQuery] = useState<any>("");
-  const [stockData, setStockData] = useState<any>(null);
+  const [stockData, setStockData] = useState<any>([]);
+  const [highestValue, setHighestValue] = useState<any>(0);
+  const [retrieveSymbol, setRetrieveSymbol] = useState<any>("");
 
-  
   const handleSearch = async () => {
-    console.log("Search Query",searchQuery)
+    /* to mock Data*/
+    // await fetchData("./mocks/stock-quote.json", "GET");
+    setError(null);
+    setStockData([]);
     await fetchData(
-      `https://finnhub.io/api/v1/quote?symbol=${searchQuery}&token=${process.env.NEXT_PUBLIC_FIN_API_KEY}`,
+      `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${searchQuery}&apikey=${process.env.NEXT_PUBLIC_ALPHAVANTAGE_API_KEY}`,
       "GET"
     );
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
-    console.log("Event",event.target.value)
-    
-  
+  };
+
+  const handleChartData = useCallback(() => {
+    const keyCheck = Object.keys(data);
+    if (keyCheck.includes("Error Message")) {
+      // Handle error case
+      setIsLoading(false);
+      setError(data?.["Error Message"]);
+      return;
     }
+    else{
+      //Retrieve Symbol
+
+      //Retrieve Search Symbol
+      setRetrieveSymbol(data?.["Meta Data"]?.["2. Symbol"]);
 
 
-  useEffect(() => {
-    setStockData(data?.c);
+    const timeSeries = data?.["Monthly Time Series"];
+    
+    if (timeSeries) {
+      const dateTime = Object.keys(timeSeries); //Obj in array
+      console.log("Date Time", dateTime)
+      const values = Object.values(timeSeries); //Obj in array
+      console.log("Values", values)
+      const closeValues = values.map((value: any) => value["4. close"]);
+      console.log("Close Values", closeValues)
+      const chartData = dateTime.map((dateTime: any, index: number) => ({
+        month: dateTime,
+        closePrice: closeValues[index],
+      }));
+
+      //Retrieve Highest Values
+      const highestValue = Math.max(...closeValues);
+      setHighestValue(highestValue);
+
+      const sortedChartData = chartData.sort(
+        (a: any, b: any) =>
+          new Date(a.month).getTime() - new Date(b.month).getTime()
+      );
+      setStockData(sortedChartData);
+      setIsLoading(false);
+    }
+  }
   }, [data]);
 
-  return (    
+  useEffect(() => {
+    if (data) {
+      setTimeout(handleChartData, 5000);
+      setIsLoading(true);
+    }
+  }, [data]);
+
+  return (
     <div className="flex flex-col justify-start ml-70 mt-10 ">
-      <h1>My Stocks</h1>
-      <p className="text-gray-500 mt-3">Search for stocks</p>
-      <Input className="w-65 mt-2" placeholder="Search for stocks" onChange={handleChange} />
-      <Button className="mt-2 w-65" onClick={handleSearch}>
-        Search
-      </Button>
-      <div>
-        <h2>
-          Current Stock: <span>{stockData}</span>
-        </h2>
+      <p className="text-xl font-bold mt-3">Search for stocks</p>
+      <div className="flex flex-row gap-6 mt-6">
+        <Input
+          className="w-45"
+          placeholder="Search for stocks"
+          onChange={handleChange}
+        />
+        <Button className=" w-20" onClick={handleSearch}>
+          Search
+        </Button>
+      </div>
+
+      <div className="mt-15 w-9/10 mx-auto">
+        {isLoading && (
+          <p className="text-xl font-bold text-center">Loading...</p>
+        )}
+        {error && (
+          <p className="text-xl font-bold text-center text-red-500">
+            Error: {error.toString()}
+          </p>
+        )}
+        {stockData.length > 0 && !isLoading && (
+          <>
+            <Card>
+              <CardHeader>
+                <CardTitle>Stock Price {retrieveSymbol}</CardTitle>
+                <CardDescription>January - June 2024</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ChartContainer config={chartConfig} className="h-900px]">
+                  <LineChart
+                    accessibilityLayer
+                    data={stockData}
+                    margin={{
+                      left: 10,
+                      right: 10,
+                    }}
+                  >
+                    <CartesianGrid vertical={true} />
+                    <XAxis
+                      dataKey="month"
+                      tickLine={false}
+                      axisLine={false}
+                      tickMargin={10}
+                      // tickFormatter={(value) => value.slice(0, 3)}
+                    />
+                    <YAxis domain={[0, highestValue + 50]} />{" "}
+                    {/* Control Y-axis max here */}
+                    <ChartTooltip
+                      cursor={false}
+                      content={<ChartTooltipContent />}
+                    />
+                    <Line
+                      dataKey="closePrice"
+                      type="natural"
+                      stroke="var(--color-closePrice)"
+                      strokeWidth={3}
+                      dot={false}
+                    />
+                  </LineChart>
+                </ChartContainer>
+              </CardContent>
+            </Card>
+          </>
+        )}
       </div>
 
       <Sidebar />
